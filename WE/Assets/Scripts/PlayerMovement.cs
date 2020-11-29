92a2379f607f23210e619441ec9ad93e960db1d1
Neele-Be <neele.bunjes@gmail.com>
2020-11-29 16:38:00 +0100

    Bunker Map: Added Interactive Objects
    - Added Scripts:
    Interactable (abstract class)
    Openable
    
    -Modified Player Movement Script; added Interaction. On Button E; the Player will interact with Objects, that has the Interactable class
    
    - Added Interactive Ojects in Map (Openable):
    Starterchest, Shelf, Research Table, and the CryoBoxes. Need to add Dialogue next

--- WE/Assets/Scripts/PlayerMovement.cs
+++ WE/Assets/Scripts/PlayerMovement.cs
@@ -8,24 +8,71 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     public Animator animator;
 
-    Vector2 movement;
+    public GameObject interactIcon;
+    private Vector2 boxSize = new Vector2(1f, 1f);
+
+    private Vector2 movement;
+
+
+    // Start
+
+    private void Start()
+    {
+        interactIcon.SetActive(false);
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        // Input
+        // Input movement
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        // Input Interaction
+
+        if (Input.GetKeyDown(KeyCode.E))
+            CheckInteractIcon();
     }
+
 
     private void FixedUpdate()
     {
         // Movement - hier, weil nicht abhängig von der Framerate
 
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    // Interaktion mit Objekten
+
+    public void OpenInteractableIcon()
+    {
+        interactIcon.SetActive(true);
+    }
+
+    public void CloseInteractableIcon()
+    {
+        interactIcon.SetActive(false);
+    }
+
+    private void CheckInteractIcon()
+    {
+        RaycastHit2D[] hits = Physics2D.BoxCastAll(transform.position,boxSize, 0, Vector2.zero);
+
+        if(hits.Length > 0)
+        {
+            foreach(RaycastHit2D rc in hits)
+            {
+                if(rc.transform.GetComponent<Interactable>())
+                {
+                    rc.transform.GetComponent<Interactable>().Interact();
+                    return;
+                }
+            }
+        }
     }
 }
