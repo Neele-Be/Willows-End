4fad1ed8476bacab29aeff9ed943c5ee8dc4c4fa
Neele-Be <neele.bunjes@gmail.com>
2020-12-03 22:05:32 +0100

    Adding Cursor Test Assets; Fixing Layer Problem; Adjusting Edge Colliders

---
+++ WE/Assets/Scripts/SpriteRendererOrderSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRendererOrderSystem : MonoBehaviour
+{
+    [SerializeField]
+    private int sortingOrdnerBase = 5000;
+    [SerializeField]
+    private int offset = 0;
+    private Renderer PositionRenderer;
+
+    private void Awake()
+    {
+        PositionRenderer = gameObject.GetComponent<Renderer>();
+
+        
+    }
+
+    private void LateUpdate()
+    {
+        PositionRenderer.sortingOrder = (int)(sortingOrdnerBase - transform.position.y - offset);
+    }
+}
