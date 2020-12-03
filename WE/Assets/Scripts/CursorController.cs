741b7865a1736754fcff7f6750d7dacf6e556d47
Neele-Be <neele.bunjes@gmail.com>
2020-12-04 00:21:30 +0100

    Fixed Cursor Sprites again (Scaling Issue: x3); Added Cursor Controller + Cursor Case Scripts

---
+++ WE/Assets/Scripts/CursorController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    public static CursorController instance;
+
+    public Texture2D pointer, look, speak, aim;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(pointer, Vector2.zero, CursorMode.Auto);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ActivatePointer()
+    {
+        Cursor.SetCursor(pointer, Vector2.zero, CursorMode.Auto);
+    }
+
+
+    public void ActivateLook()
+    {
+        Cursor.SetCursor(look, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void ActivateSpeak()
+    {
+        Cursor.SetCursor(speak, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void ActivateAim()
+    {
+        Cursor.SetCursor(aim, Vector2.zero, CursorMode.Auto);
+    }
+}
