ab218e13a02f7fc87b7fd291e14aa70250339828
Neele-Be <neele.bunjes@gmail.com>
2020-12-08 13:30:09 +0100

    Set up CameraFollowSystem

--- WE/Assets/Scripts/GameHandler.cs
+++ WE/Assets/Scripts/GameHandler.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorTriggerButton : MonoBehaviour
+public class GameHandler : MonoBehaviour
 {
+    public CameraFollow cameraFollow;
+    public Transform playerTransform;
+
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        cameraFollow.Setup(() => playerTransform.position);
     }
 
     // Update is called once per frame
